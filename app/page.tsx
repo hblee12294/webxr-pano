5549bbf0d6c5894ae9e52c3764d1051c222aa2e6
Hongbin Li <hblee12294@gmail.com>
2024-07-12 18:02:29 -0700

    feat: add basic three scaffolding

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+
 import styles from './page.module.css'
+import { XRApp } from '@/components/XRApp'
 
 export default function Home() {
-  return <main className={styles.main}></main>
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const container = containerRef.current
+    if (!container) return
+
+    const app = new XRApp(container)
+    app.start()
+
+    return () => {
+      app.dispose()
+    }
+  }, [])
+
+  return <div ref={containerRef} className={styles.main}></div>
 }
